37a58d87dc72fcdc7e55487286867c6ceb1da469
mrmooby <scotthayward@gmail.com>
2016-01-17 21:49:26 -0500

    initial build

---
+++ scripts/veryfairy.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+	/* sparkling menu items */
+
+	$('.sparkles').sparkle({
+		color: ["#ffebf4", "#ffcce3", "#d6e2f5", "#adc5eb", "#ffffff"],
+		count: 15,
+		overlap: 5,
+		speed: 1,
+		minSize: 5,
+		maxSize: 10,
+		direction: "both"
+	});
+
+});
