7d7ba27c9f6d787793bee8b5e26c29be9062282b
mrmooby <scotthayward@gmail.com>
2016-01-22 08:18:22 -0500

    main page layout DONE

--- scripts/veryfairy.js
+++ scripts/veryfairy.js
@@ -5,11 +5,10 @@ $(document).ready(function() {
 	$('.sparkles').sparkle({
 		color: ["#ffebf4", "#ffcce3", "#d6e2f5", "#adc5eb", "#ffffff"],
 		count: 15,
-		overlap: 5,
+		overlap: 10,
 		speed: 1,
 		minSize: 5,
 		maxSize: 10,
 		direction: "both"
 	});
-
 });
