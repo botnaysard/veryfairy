0f8b168495758527cd21a192d8408599e4d71a9c
mrmooby <scotthayward@gmail.com>
2016-03-17 21:34:38 -0400

    mailing list popup started

--- scripts/veryfairy.js
+++ scripts/veryfairy.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	/* sparkling menu items */
 	/* NOTE: add some code that will adjust the number of sparkles based onthe width of the element */
 
-	$('.sparkles-light').sparkle({
+	$(".sparkles-light").sparkle({
 		color: ["#ffebf4", "#ffcce3", "#d6e2f5", "#adc5eb", "#ffffff"],
 		count: 15,
 		overlap: 10,
@@ -13,4 +13,22 @@ $(document).ready(function() {
 		direction: "both"
 	});
 
+	/* overlay for mailing list join */
+
+	$("#join-list").click(function(event){
+		$( "#dark-overlay" ).fadeIn( "slow", function() {
+			$("#mailinglist-popup").show();
+        	$("#close-overlay" ).show();
+        	$("#close-overlay" ).click(function(){
+        		$("#mailinglist-popup").hide();
+        		$("#dark-overlay" ).hide();
+        		$("#close-overlay" ).hide();
+    		});
+  		});
+	});
+
+
+
+
+	
 });
