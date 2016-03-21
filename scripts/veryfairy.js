e3d03ae0d9b26d264a5163bb1d598efa3b3e1241
mrmooby <scotthayward@gmail.com>
2016-03-21 13:52:50 -0400

    mail signup working

--- scripts/veryfairy.js
+++ scripts/veryfairy.js
@@ -13,6 +13,17 @@ $(document).ready(function() {
 	/* END - SPARKLE ON HIGHLIGHT */
 
 	/* START - MAILING LIST POP-UP */
+
+	$(function () {
+		var $form = $('#mc-embedded-subscribe-form');
+		$('#mc-embedded-subscribe').on('click', function(event) {
+	    	if(event) event.preventDefault();
+	    	register($form);
+	  	});
+	});
+
+
+
 	$("#join-list").click(function(event){
 		$( "#dark-overlay" ).fadeIn( "slow", function() {
 			$("#mc_embed_signup").show();
@@ -24,6 +35,33 @@ $(document).ready(function() {
     		});
   		});
 	});
-	/* END - SPARKLE ON HIGHLIGHT */
+	/* END - MAILING LIST POP-UP*/
+
+	/* START - MAILING LIST AJAX */
+	function register($form) {
+		$.ajax({
+		    type: $form.attr('method'),
+		    url: $form.attr('action'),
+		    data: $form.serialize(),
+		    cache       : false,
+		    dataType    : 'json',
+		    contentType: "application/json; charset=utf-8",
+		    error       : function(err) { $('#notification_container').html('<span class="alert">Could not connect to server. Please try again later.</span>'); },
+		    success     : function(data) {
+	   
+				if (data.result != "success") {
+					var message = data.msg.substring(4);
+					$('#notification_container').html('<span class="alert">'+'ERROR: '+ message +'</span>');
+				} 
+
+				else {
+					var message = data.msg;
+					$('#mc_embed_signup form').html('<span class="success">'+'SUCCESS! '+ message +'</span>');
+					$('#notification_container').hide();
+				}
+	    	}
+	  	});
+	}
+	/* END - MAILING LIST AJAX */
 
 });
