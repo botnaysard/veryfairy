8a70e512588761033fd342138d35a72fa9d324d6
botnaysard <scotthayward@gmail.com>
2016-12-01 18:25:10 -0500

    main page slider coding done add to other pages

--- scripts/veryfairy.js
+++ scripts/veryfairy.js
@@ -166,4 +166,18 @@ $(document).ready(function() {
 
 	/* END CONTACT FORM AJAX */
 
+	/* START IMAGE SLIDER FOR HEADER */
+
+	$(".slider > div:gt(0)").hide();
+		setInterval(function() { 
+  			$('.slider > div:first')
+    		.fadeOut(1000)
+    		.next()
+    		.fadeIn(1000)
+    		.end()
+    		.appendTo('.slider');
+		},  5000);
+
+	/* END IMAGE SLIDER FOR HEADER */
+
 });
